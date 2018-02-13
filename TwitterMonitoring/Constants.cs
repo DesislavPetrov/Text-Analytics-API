5a8d65b56c05c689db04079a315682e16f186d12
Desislav Petrov <petrov_desislav@abv.bg>
2018-02-13 23:46:28 +0200

    Update Constants.cs

--- TwitterMonitoring/Constants.cs
+++ TwitterMonitoring/Constants.cs
@@ -2,11 +2,11 @@
 {
     public static class Constants
     {
-        public const string ApiKey = "3e563a7956224862888415a4105b44a8";
-        public const string TwitterConsumerKey = "n1F9WKf3vZtdzw65UHMsfGBqp";
-        public const string TwitterConsumerSecret = "5OaV3jQvyo3BNDkrSmWyMPmvUkb4UwxdinLs0SBi75H4c80zw7";
-        public const string TwitterAccessToken = "1222543662-gBsm7EOwQyQrQCxucoanSkpKqVpcgmRmX4K0n2d";
-        public const string TwitterTokenSecret = "ksYkTQk4Ov24FLi8SUMscx1b8DYtxbY00ZJHM7O7y07FU";
+        public const string ApiKey = "*********************";
+        public const string TwitterConsumerKey = "********************";
+        public const string TwitterConsumerSecret = "**************************";
+        public const string TwitterAccessToken = "***********************";
+        public const string TwitterTokenSecret = "***********************";
     }
 }
 
